6f8b08f972255d21e0c06d4ad3cc71f071b7fa63
nbsistema <nbconsultoriasistema@gmail.com>
2025-08-25 18:02:44 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+const path = require("path");
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
